ba884ea950a72ee1535f56477b6dd2c48df8748b
alexcompton <alexcompton11@gmail.com>
2015-05-06 20:25:46 -0400

    properties

---
+++ Properties/ParameterfulProperties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterfulProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n\npress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
